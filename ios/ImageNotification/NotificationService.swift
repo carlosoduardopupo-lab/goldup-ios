b4edefb2ee01d885f3d9e4f9e5553d8f998adedf
carlosoduardopupo-lab <carlosoduardopupo@gmail.com>
2026-04-30 02:15:21 -0700

    Update NotificationService.swift

--- ios/ImageNotification/NotificationService.swift
+++ ios/ImageNotification/NotificationService.swift
@@ -1,4 +1,3 @@
-
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
@@ -6,22 +5,20 @@ class NotificationService: UNNotificationServiceExtension {
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    override func didReceive(_ request: UNNotificationRequest,
+                             withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
-        bestAttemptContent = request.content
-          .mutableCopy() as? UNMutableNotificationContent
-        guard let bestAttemptContent = bestAttemptContent else { return }
-        FIRMessagingExtensionHelper().populateNotificationContent(
-          bestAttemptContent,
-          withContentHandler: contentHandler)
-    }
-    
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+        bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent
+
+        if let bestAttemptContent = bestAttemptContent {
             contentHandler(bestAttemptContent)
         }
     }
 
+    override func serviceExtensionTimeWillExpire() {
+        if let contentHandler = contentHandler,
+           let bestAttemptContent = bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
 }
